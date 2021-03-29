ed0324a76aeabb961cf7597a26d46705dacf77c4
Ester Gama <esterfaniasantos@gmail.com>
2021-03-28 22:24:21 -0300

    add TransactionTable component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import { Summary } from "../Summary";
 import { Container } from "./styles";
+import { TransactionTable } from "../TransactionTable/";
 export function Dashboard() {
     return (
         <Container>
             <Summary />
+            <TransactionTable />
         </Container>
     );
 }
